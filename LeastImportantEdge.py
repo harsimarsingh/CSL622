889c37fe74a9248babb4338d98dd959c989d16b5
Harsimar Singh <harsimar_saini@hotmail.com>
2018-08-20 12:49:45 +0530

    Update LeastImportantEdge.py

--- LeastImportantEdge.py
+++ LeastImportantEdge.py
@@ -1,4 +1,7 @@
-#Collaborators- Harsimar Singh, Subhranil Bagchi, Anurag Banerjee
+#Collaborators- 
+#Harsimar Singh 2018csm1010
+#Subhranil Bagchi
+#Anurag Banerjee
 
 #In this module we will find the weak link and then make it a strong
 #link in the graph and it is similar to the increasing influence
@@ -58,4 +61,10 @@ leader=leader(G)
 connect(G,weak,leader)
 
 print (G.degree())
-print ("degree of weak node after connecting to neighbour of leader node is " + str(G.degree(weak)))		
+print ("degree of weak node after connecting to neighbour of leader node is " + str(G.degree(weak)))
+
+#In the next assignment the changes will be 
+#1. The Graph will be changed to directed
+#2. The leader function and weak link fucntion will be modified
+#3. Some extra parameters will be added (which will complicate the algorithm) such as some credential or weights.This will mark
+#   importance of the node as we have in real world problems- "The an important page in web pages of world wide web".
